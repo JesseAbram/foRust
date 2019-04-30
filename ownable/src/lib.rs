d2a7db0e837cad032bdb3796d8bb63dab0deade4
Jesse Abramowitz <jesseabramowitz@hotmail.com>
2019-04-30 15:11:11 -0400

    added tests and more functions

--- ownable/src/lib.rs
+++ ownable/src/lib.rs
@@ -1,12 +1,10 @@
 #![cfg_attr(not(any(test, feature = "std")), no_std)]
 
-use ink_core::{
-    env::println,
-    memory::format,
-    storage,
-};
 use ink_lang::contract;
-use ink_core::env::AccountId;
+use ink_core::storage;
+use ink_core::memory::format;
+use ink_core::env::{self, println, AccountId};
+
 
 
 contract! {
@@ -16,6 +14,7 @@ contract! {
     }
 
     impl Deploy for Ownable {
+        
         fn deploy(&mut self) {
             self.owner.set(env.caller());
         }
@@ -23,46 +22,76 @@ contract! {
 
     impl Ownable {
 
-    /** 
-     * @return true if `msg.sender` is the owner of the contract.
-     */       
-    // fn is_owner(&mut self) -> bool {
-        
-    //     }
+    
+        pub (external) fn is_owner(&mut self) -> bool {
+            env::caller() == *self.owner 
+         }
 
-    /**
-     * @dev Allows the current owner to relinquish control of the contract.
-     * It will not be possible to call the functions with the `onlyOwner`
-     * modifier anymore.
-     * @notice Renouncing ownership will leave the contract without an owner,
-     * thereby removing any functionality that is only available to the owner.
-     */
-    //  fn renounce_ownership(&mut self) {
+       pub (external) fn get_owner(&mut self) -> AccountId {
+           *self.owner
+       }
 
-    //  }
+       fn only_owner(&mut self, caller: AccountId) -> bool {
+           if caller == *self.owner {
+               true
+           } else {
+               panic!("Must be called from owner account");
+           }
+       }
 
-         
-    /**
-     * @dev Allows the current owner to transfer control of the contract to a newOwner.
-     * @param newOwner The address to transfer ownership to.
-     */
-
-
-    /**
-     * @dev Transfers control of the contract to a newOwner.
-     * @param newOwner The address to transfer ownership to.
-     */
-
-      
+       pub (external) fn transfer_ownership(&mut self, new_owner: AccountId){
+           self.only_owner(env::caller());
+           self.owner.set(new_owner); 
+       }
+    
     }
 }
 
 #[cfg(all(test, feature = "test-env"))]
 mod tests {
     use super::*;
+    use std::convert::TryFrom;
+
+      
+    #[test]
+
+    fn is_owner() {
+        let bob = AccountId::try_from([0x1; 32]).unwrap();
+
+        let mut contract = Ownable::deploy_mock();
+        assert_eq!(contract.is_owner(), true);
+        env::test::set_caller(bob);
+        assert_eq!(contract.is_owner(), false);
+
+    }
+    
+    #[test]
+
+    fn get_owner() {
+        let alice = AccountId::try_from([0x0; 32]).unwrap();
+        let bob = AccountId::try_from([0x1; 32]).unwrap();
+
+
+        let mut contract = Ownable::deploy_mock();
+        assert_eq!(contract.get_owner(), alice);
+        assert_ne!(contract.get_owner(), bob);
+
+
+    }
 
     #[test]
-    fn it_works() {
-    
+
+    fn transfer_ownership() {
+        let alice = AccountId::try_from([0x0; 32]).unwrap();
+        let bob = AccountId::try_from([0x1; 32]).unwrap();
+
+        let mut contract = Ownable::deploy_mock();
+        contract.transfer_ownership(bob);
+        // will fail
+        // contract.transfer_ownership(alice);        
+      
+
     }
+
+
 }
